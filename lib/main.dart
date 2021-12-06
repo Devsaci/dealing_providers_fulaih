df6bbae8ab5e628d9304c97c75e0372f0f9a66a1
saci zakaria <sacideveloppeur@gmail.com>
2021-12-06 18:44:20 +0100

    provider / ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
-
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: true,
+      home: ChangeNotifierProvider(
+        create: (BuildContext context) {},
+        child: MyHomePage(),
+      ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/my_provider.dart
+++ lib/my_provider.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyProvider with ChangeNotifier {
+mixin MyProvider implements ChangeNotifier {
   int _count = 0;
 
   int get count => _count;
