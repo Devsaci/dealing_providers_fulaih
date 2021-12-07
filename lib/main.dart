3cdc94a883102bd2eb03b88c53fbdff896d88817
saci zakaria <sacideveloppeur@gmail.com>
2021-12-07 09:58:12 +0100

    SecondMethode

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //SecondMethode
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ChangeNotifierProvider(
